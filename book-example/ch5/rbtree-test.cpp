1e929dd97ec3da4a1a6f85204d95be9d9e1fb90e
xtx <13151562558@163.com>
2019-04-14 15:31:27 +0800

    放弃包含SGI-STL源码的头文件

--- book-example/ch5/rbtree-test.cpp
+++ book-example/ch5/rbtree-test.cpp
@@ -1,12 +1,14 @@
+// 编译程序链接SGI-STL源码，编译问题很多
+// 在linux中编译 指定 /usr/include/c++/5/ 中的头文件，编译时成功的
 #include <iostream>
 
 #include "stl_tree.h"
 #include "stl_function.h"
 
-using namespace std;
-
 int main()
 {
-    _Rb_tree<int, int, _Identity<int>, less<int> > itree;
+    _Rb_tree<int, int, _Identity<int>, less<int>>
+        itree;
     cout << itree.size() << endl;
+    return 0;
 }
